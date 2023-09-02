44d849fa0ef5b4f729be67f42199f4c87f7cff20
Roger Tan <rtan4955@uni.sydney.edu.au>
2023-09-02 10:08:53 +1000

    f

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,22 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar1 from "@/components/Navbar1";
 import Landing1 from "@/components/Landing1";
+import Features from "@/components/Features";
+import Reviews from "@/components/Reviews";
+import Numbers from "@/components/Numbers";
+import Footer from "@/components/Footer";
+import AuthModal from "@/components/AuthModal"
 
 export default function Home() {
   return (
     <>
+      <AuthModal />
       <Navbar1 />
       <Landing1 />
+      <Features />
+      <Reviews />
+      <Numbers />
+      <Footer />
     </>
   );
 }
---
+++ components/AuthModal.js
@@ -0,0 +1,19 @@
+import { Modal } from "@mui/material";
+
+function AuthModal() {
+  <>
+    <Modal open={true}>
+      <h2>Log in to Summarist</h2>
+      <h3>Login as Guest</h3>
+      <h3>or</h3>
+      <h3>Login with Google</h3>
+      <input />
+      <input />
+      <button>Login</button>
+      <button>Forgot your password</button>
+      <button>Don't have an account </button>
+    </Modal>
+  </>;
+}
+
+export default AuthModal;
