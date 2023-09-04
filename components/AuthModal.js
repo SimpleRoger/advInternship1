a59cc45de20379325a69f600974cb1c891af8186
Roger Tan <rtan4955@uni.sydney.edu.au>
2023-09-04 13:04:30 +1000

    f

--- components/AuthModal.js
+++ components/AuthModal.js
@@ -1,19 +1,21 @@
-import { Modal } from "@mui/material";
+import Modal from "@mui/material/Modal";
 
 function AuthModal() {
-  <>
-    <Modal open={true}>
-      <h2>Log in to Summarist</h2>
-      <h3>Login as Guest</h3>
-      <h3>or</h3>
-      <h3>Login with Google</h3>
-      <input />
-      <input />
-      <button>Login</button>
-      <button>Forgot your password</button>
-      <button>Don't have an account </button>
-    </Modal>
-  </>;
+  return (
+    <>
+      <Modal open={true}>
+        <h2>Log in to Summarist</h2>
+        <h3>Login as Guest</h3>
+        <h3>or</h3>
+        <h3>Login with Google</h3>
+        <input />
+        <input />
+        <button>Login</button>
+        <button>Forgot your password</button>
+        <button>Don't have an account </button>
+      </Modal>
+    </>
+  );
 }
 
 export default AuthModal;
